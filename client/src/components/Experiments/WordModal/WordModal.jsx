195d8c1d43604330af4f4a337091a7390f3ae12e
Harsh <harsh.ju.sharma@gmail.com>
2024-06-19 22:41:34 +0530

    Word Modal - final

--- client/src/components/Experiments/WordModal/WordModal.jsx
+++ client/src/components/Experiments/WordModal/WordModal.jsx
@@ -4,57 +4,72 @@ import "./WordModal.css";
 import SOUND_POP from "../../../assets/SOUND_POP.mp3";
 import useClickOutside from "../../../hooks/useClickOutside";
 
-const WordModal = ({ boundingRef, word, children }) => {
+const WordModal = ({ boundingRef, word, children, muted = false }) => {
   const [isModalOpen, setIsModalOpen] = React.useState(false);
   const buttonRef = React.useRef(null);
+  const rmRef = React.useRef(null);
 
   const [modalPos, setModalPos] = React.useState();
 
   const MODAL_WIDTH = 300;
   const MODAL_HEIGHT = 200;
-  const BLUR = "blur(0px)";
+  const BLUR = "blur(2px)";
   const success_audio = new Audio(SOUND_POP);
+
+  React.useEffect(() => {
+    const handleEsc = (event) => {
+      if (event.key === "Escape") {
+        myFunction();
+      }
+    };
+    window.addEventListener("scroll", closeModal);
+    window.addEventListener("keydown", handleEsc);
+
+    return () => {
+      window.removeEventListener("scroll", closeModal);
+      window.removeEventListener("keydown", handleEsc);
+    };
+  }, []);
 
   const closeModal = () => {
     setIsModalOpen(false);
-    if (boundingRef) boundingRef.current.style.filter = "none";
-    else {
-      document.body.style.filter = "none";
-    }
   };
 
   const handleButtonClick = () => {
     if (isModalOpen) closeModal();
     else {
-      computeModalPosition();
-      success_audio.play();
+      computeModalPos();
+      if (!muted) success_audio.play();
       setIsModalOpen(true);
     }
   };
 
-  useClickOutside(buttonRef, closeModal);
+  useClickOutside(rmRef, closeModal);
 
-  const computeModalPosition = () => {
+  const computeModalPos = () => {
     if (!buttonRef) return;
+
     const buttonRect = buttonRef.current.getBoundingClientRect();
+    const boundingRect = boundingRef
+      ? boundingRef.current.getBoundingClientRect()
+      : {
+          left: 0,
+          right: window.innerWidth,
+          top: 0,
+          bottom: window.innerHeight,
+          width: window.innerWidth,
+          height: window.innerHeight,
+        };
 
-    let SPACE_WIDTH = boundingRef
-      ? boundingRef.current.getBoundingClientRect().width
-      : window.innerWidth;
+    const SPACE_WIDTH = boundingRect.width;
+    const SPACE_HEIGHT = boundingRect.height;
 
-    let SPACE_HEIGHT = boundingRef
-      ? boundingRef.current.getBoundingClientRect().height
-      : window.innerHeight;
+    let GRIDS = getGrids(boundingRect);
 
-    let GRIDS = window.innerWidth > 600 ? 3 : window.innerWidth > 450 ? 3 : 2;
+    let SPACE_PADDING_WIDTH = boundingRect.width / GRIDS;
 
-    let SPACE_WIDTH_PADDING = SPACE_WIDTH / GRIDS;
-    let SPACE_HEIGHT_PADDING = Math.max(SPACE_HEIGHT / GRIDS, 200);
-
-    if (boundingRef) boundingRef.current.style.filter = BLUR;
-    else {
-      document.body.style.filter = BLUR;
-    }
+    let BUTTON_CENTER =
+      buttonRect.left - boundingRect.left + buttonRect.width / 2;
 
     let verticalPos = { top: buttonRect.height };
     let def = boundingRef
@@ -65,61 +80,62 @@ const WordModal = ({ boundingRef, word, children }) => {
       : window.innerHeight;
 
     let isFlipped = false;
-    if (Math.abs(buttonRect.bottom - def) < 220) {
+    if (Math.abs(buttonRect.bottom - def) < MODAL_HEIGHT + 20) {
       isFlipped = true;
       verticalPos = { bottom: buttonRect.height };
     }
-    let RIGHT_LIMIT = boundingRef
-      ? boundingRef.current.getBoundingClientRect().right
-      : window.innerWidth;
 
-    let LEFT_LIMIT = boundingRef
-      ? boundingRef.current.getBoundingClientRect().left
-      : 0;
+    // console.log({
+    //   SPACE_WIDTH,
+    //   SPACE_PADDING_WIDTH,
+    //   GRIDS,
+    //   left: buttonRect.left - boundingRect.left,
+    //   rightEnd: (GRIDS - 1) * SPACE_PADDING_WIDTH,
+    //   BUTTON_CENTER,
+    // });
 
-    if (buttonRect.left < SPACE_WIDTH_PADDING) {
-      console.log("Left");
-      if (buttonRect.left + MODAL_WIDTH > RIGHT_LIMIT) {
-        console.log("adjusting from left");
-        let delta_adj = buttonRect.left + MODAL_WIDTH - RIGHT_LIMIT + 16;
+    if (BUTTON_CENTER < SPACE_PADDING_WIDTH) {
+      // console.log("Left");
+
+      if (buttonRect.left + MODAL_WIDTH > boundingRect.right) {
+        let DELTA_ADJ = buttonRect.left + MODAL_WIDTH - boundingRect.right + 16;
 
         setModalPos({
-          left: -1 * delta_adj,
+          left: -1 * DELTA_ADJ,
           transformOrigin: isFlipped ? "bottom left" : "top left",
           ...verticalPos,
         });
 
         return;
       }
-
       setModalPos({
         left: 0,
         transformOrigin: isFlipped ? "bottom left" : "top left",
         ...verticalPos,
       });
-    } else if (buttonRect.left > (GRIDS - 1) * SPACE_WIDTH_PADDING) {
-      console.log("Right");
-      if (buttonRect.right - MODAL_WIDTH < LEFT_LIMIT) {
-        console.log("Adjusting");
-        let delta_adj = LEFT_LIMIT - buttonRect.right + MODAL_WIDTH + 16;
+    } else if (BUTTON_CENTER > (GRIDS - 1) * SPACE_PADDING_WIDTH) {
+      // console.log("Right");
 
+      if (buttonRect.right - MODAL_WIDTH < boundingRect.left - 16) {
+        let DELTA_ADJ = boundingRect.left - buttonRect.right + MODAL_WIDTH + 16;
         setModalPos({
-          right: -1 * delta_adj,
+          right: -1 * DELTA_ADJ,
           transformOrigin: isFlipped ? "bottom right" : "top right",
           ...verticalPos,
         });
 
         return;
       }
-
       setModalPos({
         right: 0,
         transformOrigin: isFlipped ? "bottom right" : "top right",
         ...verticalPos,
       });
     } else {
-      console.log("Middle");
+      // console.log("Middle");
+
       setModalPos({
+        left: "50%",
         transform: "translateX(-50%)",
         transformOrigin: isFlipped ? "bottom left" : "top left",
         ...verticalPos,
@@ -127,8 +143,18 @@ const WordModal = ({ boundingRef, word, children }) => {
     }
   };
 
+  const getGrids = (boundingRect) => {
+    let width = boundingRect.width;
+
+    if (width < 500) return 2;
+
+    if (width > 800) return 4;
+
+    return 3;
+  };
+
   return (
-    <span style={{ filter: "10px !important" }} className="wm-wrap">
+    <div ref={rmRef} style={{ filter: "10px !important" }} className="wm-wrap">
       <button
         ref={buttonRef}
         onClick={handleButtonClick}
@@ -137,21 +163,121 @@ const WordModal = ({ boundingRef, word, children }) => {
         {word}
       </button>
 
+      {isModalOpen && <div className="wm-underlay"></div>}
+
       {isModalOpen && (
         <span
           style={{
             ...modalPos,
             width: MODAL_WIDTH,
-            height: MODAL_HEIGHT,
-            // top: buttonRef.current.getBoundingClientRect().height,
+            maxHeight: MODAL_HEIGHT,
           }}
           className="wm-modal"
         >
           {children}
         </span>
       )}
-    </span>
+    </div>
   );
 };
 
 export default WordModal;
+
+// const computeModalPosition = () => {
+//   if (!buttonRef) return;
+//   const buttonRect = buttonRef.current.getBoundingClientRect();
+
+//   let SPACE_WIDTH = boundingRef
+//     ? boundingRef.current.getBoundingClientRect().width
+//     : window.innerWidth;
+
+//   let SPACE_HEIGHT = boundingRef
+//     ? boundingRef.current.getBoundingClientRect().height
+//     : window.innerHeight;
+
+//   let GRIDS = window.innerWidth > 600 ? 4 : window.innerWidth > 450 ? 3 : 2;
+
+//   let SPACE_WIDTH_PADDING = SPACE_WIDTH / GRIDS;
+//   let SPACE_HEIGHT_PADDING = Math.max(SPACE_HEIGHT / GRIDS, 200);
+
+//   // if (boundingRef) boundingRef.current.style.filter = BLUR;
+//   // else {
+//   //   document.body.style.filter = BLUR;
+//   // }
+//   console.log({
+//     SPACE_WIDTH,
+//     SPACE_WIDTH_PADDING,
+//     buttonLeft: buttonRect.left,
+//   });
+
+//   let verticalPos = { top: buttonRect.height };
+//   let def = boundingRef
+//     ? Math.min(
+//         window.innerHeight,
+//         boundingRef.current.getBoundingClientRect().bottom
+//       )
+//     : window.innerHeight;
+
+//   let isFlipped = false;
+//   if (Math.abs(buttonRect.bottom - def) < MODAL_HEIGHT + 20) {
+//     isFlipped = true;
+//     verticalPos = { bottom: buttonRect.height };
+//   }
+//   let RIGHT_LIMIT = boundingRef
+//     ? boundingRef.current.getBoundingClientRect().right
+//     : window.innerWidth;
+
+//   let LEFT_LIMIT = boundingRef
+//     ? boundingRef.current.getBoundingClientRect().left
+//     : 0;
+
+//   if (buttonRect.left < SPACE_WIDTH_PADDING) {
+//     console.log("Left");
+//     if (buttonRect.left + MODAL_WIDTH > RIGHT_LIMIT) {
+//       console.log("adjusting from left");
+//       let delta_adj = buttonRect.left + MODAL_WIDTH - RIGHT_LIMIT + 16;
+
+//       setModalPos({
+//         left: -1 * delta_adj,
+//         transformOrigin: isFlipped ? "bottom left" : "top left",
+//         ...verticalPos,
+//       });
+
+//       return;
+//     }
+
+//     setModalPos({
+//       left: 0,
+//       transformOrigin: isFlipped ? "bottom left" : "top left",
+//       ...verticalPos,
+//     });
+//   } else if (buttonRect.left > (GRIDS - 1) * SPACE_WIDTH_PADDING) {
+//     console.log("Right");
+//     if (buttonRect.right - MODAL_WIDTH < LEFT_LIMIT) {
+//       console.log("Adjusting");
+//       let delta_adj = LEFT_LIMIT - buttonRect.right + MODAL_WIDTH;
+
+//       setModalPos({
+//         right: -1 * (delta_adj + 16),
+//         transformOrigin: isFlipped ? "bottom right" : "top right",
+//         ...verticalPos,
+//       });
+
+//       return;
+//     }
+
+//     setModalPos({
+//       right: 0,
+//       transformOrigin: isFlipped ? "bottom right" : "top right",
+//       ...verticalPos,
+//     });
+//   } else {
+//     console.log("Middle");
+//     setModalPos({
+//       // left: "50%",
+//       transform: "translateX(-50%)",
+//       transformOrigin: isFlipped ? "bottom left" : "top left",
+//       ...verticalPos,
+//     });
+//   }
+// };
--- client/src/pages/Experiments/WordModalPage/WordModalPage.jsx
+++ client/src/pages/Experiments/WordModalPage/WordModalPage.jsx
@@ -3,6 +3,8 @@ import "./WordModalPage.css";
 import ExpPageTitle from "../../../components/ExpPageTitle/ExpPageTitle";
 import WordModal from "../../../components/Experiments/WordModal/WordModal";
 
+import { Speech } from "lucide-react";
+
 const WordModalPage = () => {
   const demoRef = React.useRef(null);
   return (
@@ -10,87 +12,147 @@ const WordModalPage = () => {
       <ExpPageTitle title="Word Modal" date="June 2024" />
       <div ref={demoRef} className="word-modal-demo">
         <div>
-          In the{" "}
-          <WordModal word="countenance" boundingRef={demoRef}>
-            <Dictionary />
+          In the tranquil embrace of solitude, amidst the verdant{" "}
+          <WordModal boundingRef={demoRef} word="tapestry">
+            <Dictionary
+              word="tapestry"
+              pronunciation="/ˈtæpəstri/"
+              type="noun"
+              meaning="a heavy cloth that has designs or pictures woven into it and that is used for wall hangings, curtains, etc."
+              usage="The tapestry on the castle walls depicted scenes from ancient battles."
+            />
           </WordModal>{" "}
-          embrace of solitude, amidst the verdant tapestry of a riverside grove,
-          there sat a <WordModal word="soul"></WordModal> in poignant
-          contemplation. Here, where the{" "}
-          <WordModal word="countenance" boundingRef={demoRef}></WordModal> of
-          the flowing{" "}
-          <WordModal word="countenance" boundingRef={demoRef}></WordModal>
-          whispered secrets to the sylvan sentinels, and where the caress of
-          <WordModal word="countenance" boundingRef={demoRef}></WordModal>{" "}
+          of a riverside grove, there sat a soul in poignant contemplation.
+          Here, where the murmurs of the flowing stream whispered secrets to the{" "}
+          <WordModal boundingRef={demoRef} word="sylvan">
+            <Dictionary
+              word="sylvan"
+              pronunciation="/ˈsɪlvən/"
+              type="adjective"
+              meaning="relating to or characteristic of woods or forests"
+              usage="The sylvan scenery of the national park was breathtaking. \n The sylvan scenery of the national park was breathtaking. "
+            />
+          </WordModal>{" "}
+          sentinels, and where the caress of{" "}
+          <WordModal boundingRef={demoRef} word="zephyr">
+            <Dictionary
+              word="zephyr"
+              pronunciation="/ˈzɛfər/"
+              type="noun"
+              meaning="a gentle, mild breeze"
+              usage="The zephyr rustled the leaves in the quiet afternoon."
+            />
+          </WordModal>{" "}
           carried the fragrant hymns of blossoms in bloom, lingered a man of
-          profound introspection.
+          profound introspection. <br />
           <br />
-          <br />
-          His <WordModal word="countenance" boundingRef={demoRef}></WordModal>,
-          etched with the furrows of unrevealed burdens, bespoke a spirit
-          ensnared in the paradox of life's most{" "}
-          <WordModal word="countenance" boundingRef={demoRef}></WordModal>{" "}
-          gifts. For though the tapestry of his existence bore the woven threads
-          of prosperity and{" "}
-          <WordModal word="countenance" boundingRef={demoRef}></WordModal>, he
-          remained steadfast in resistance, a beacon of{" "}
-          <WordModal word="countenance" boundingRef={demoRef}></WordModal>{" "}
+          His{" "}
+          <WordModal boundingRef={demoRef} word="countenance">
+            <Dictionary
+              word="countenance"
+              pronunciation="/ˈkaʊntənəns/"
+              type="noun"
+              meaning="a person's face or facial expression"
+              usage="Her countenance showed a mixture of surprise and joy."
+            />
+          </WordModal>{" "}
+          , etched with the furrows of unrevealed burdens, bespoke a spirit
+          ensnared in the paradox of life's most exquisite gifts. For though the
+          tapestry of his existence bore the woven threads of prosperity and
+          serendipity, he remained steadfast in resistance, a beacon of defiance
           against the allure of joy untrammeled.
           <br />
           <br />
-          In the solace of those hallowed{" "}
-          <WordModal word="countenance" boundingRef={demoRef}></WordModal>,
-          where the cerulean waters mirrored the expanse of an azure sky, he
-          wrestled with the verities of fate and fortune. Amidst the ballet of
-          sunlight and shadow, where each verdant leaf pirouetted in the gentle{" "}
-          <WordModal word="countenance" boundingRef={demoRef}></WordModal> of
-          the breeze, he found himself ensnared in the throes of ambivalence.
+          In the solace of those hallowed riverbanks, where the cerulean waters
+          mirrored the expanse of an azure sky, he wrestled with the{" "}
+          <WordModal boundingRef={demoRef} word="verities">
+            <Dictionary
+              word="verities"
+              pronunciation="/ˈvɛrɪtiz/"
+              type="noun"
+              meaning="the quality or state of being true or real"
+              usage="He sought to understand the verities of human existence."
+            />
+          </WordModal>{" "}
+          of fate and fortune. Amidst the ballet of sunlight and shadow, where
+          each verdant leaf pirouetted in the gentle choreography of the breeze,
+          he found himself ensnared in the throes of ambivalence.
           <br />
-          <br /> For every{" "}
-          <WordModal word="countenance" boundingRef={demoRef}></WordModal> dawn
-          that kissed the dew-kissed petals awake, and every{" "}
-          <WordModal word="countenance" boundingRef={demoRef}></WordModal> that
+          <br />
+          For every{" "}
+          <WordModal boundingRef={demoRef} word="effulgent">
+            <Dictionary
+              word="effulgent"
+              pronunciation="/ɪˈfʌldʒənt/"
+              type="adjective"
+              meaning="shining brightly; radiant"
+              usage="The effulgent sun cast a warm glow over the meadow."
+            />
+          </WordModal>{" "}
+          dawn that kissed the dew-kissed petals awake, and every dusk that
           draped the world in a tapestry of saffron and gold, he recoiled from
-          the embrace of fleeting euphoria. The symphony of{" "}
-          <WordModal word="countenance" boundingRef={demoRef}></WordModal> that
+          the embrace of fleeting euphoria. The symphony of birdsong that
           orchestrated the dawn's awakening seemed but a distant echo to his
           ears, dulled by the armor of resilience he clad himself in.
           <br />
           <br />
           In the heart of that idyllic sanctuary, where time flowed as languidly
           as the river's meandering course, he was beset by an inner tempest—a
-          tempest that{" "}
-          <WordModal word="countenance" boundingRef={demoRef}></WordModal>{" "}
-          against the gilded promises of blissful{" "}
-          <WordModal word="countenance" boundingRef={demoRef}></WordModal>. Even
-          as the seasons waltzed in their eternal cycle of birth and decay, he
-          remained{" "}
-          <WordModal word="countenance" boundingRef={demoRef}></WordModal> in a
-          fortress of stoic resolve.
+          tempest that raged against the gilded promises of blissful reprieve.
+          Even as the seasons waltzed in their eternal cycle of birth and decay,
+          he remained ensconced in a fortress of{" "}
+          <WordModal boundingRef={demoRef} word="stoic">
+            <Dictionary
+              word="stoic"
+              pronunciation="/ˈstoʊɪk/"
+              type="adjective"
+              meaning="enduring pain or hardship without showing one's feelings or complaining"
+              usage="She maintained a stoic demeanor throughout the difficult ordeal."
+            />
+          </WordModal>{" "}
+          resolve.
           <br />
           <br />
-          Yet, amidst the verdure that framed his{" "}
-          <WordModal word="countenance" boundingRef={demoRef}></WordModal>{" "}
-          vigil, there bloomed a perennial hope—an incandescent ember that
-          refused to be
-          <WordModal word="countenance" boundingRef={demoRef}></WordModal>. For
-          deep within the recesses of his being, beneath the veneer of
-          adamantine restraint, lay the whispered desire to relinquish the{" "}
-          <WordModal word="countenance" boundingRef={demoRef}></WordModal> of
-          self-imposed restraint.
+          Yet, amidst the{" "}
+          <WordModal boundingRef={demoRef} word="verdure">
+            <Dictionary
+              word="verdure"
+              pronunciation="/ˈvɜːrdʒər/"
+              type="noun"
+              meaning="lush green vegetation, especially grass or herbage"
+              usage="The rolling hills were covered in verdure after the spring rains."
+            />
+          </WordModal>{" "}
+          that framed his melancholic vigil, there bloomed a perennial hope—an
+          incandescent ember that refused to be extinguished. For deep within
+          the recesses of his being, beneath the{" "}
+          <WordModal boundingRef={demoRef} word="veneer">
+            <Dictionary
+              word="veneer"
+              pronunciation="/vəˈnɪər/"
+              type="noun"
+              meaning="a thin decorative covering of fine wood applied to coarser wood or other material"
+              usage="The furniture had a veneer of mahogany that gave it a rich appearance."
+            />
+          </WordModal>{" "}
+          of adamantine restraint, lay the whispered desire to relinquish the
+          shackles of self-imposed restraint.
           <br />
           <br />
-          Thus, as the river{" "}
-          <WordModal word="countenance" boundingRef={demoRef}></WordModal> its
-          eternal{" "}
-          <WordModal word="countenance" boundingRef={demoRef}></WordModal> and
-          the stars bore witness to the symphony of his inner turmoil, he found
-          himself standing at the precipice of revelation. For in the very act
-          of resistance, he unearthed the paradox that had eluded him—a
-          revelation that the most{" "}
-          <WordModal word="countenance" boundingRef={demoRef}></WordModal>{" "}
-          beauty often resides in the surrender to life's{" "}
-          <WordModal word="countenance" boundingRef={demoRef}></WordModal>{" "}
+          Thus, as the river murmured its eternal lullaby and the stars bore
+          witness to the symphony of his inner turmoil, he found himself
+          standing at the precipice of revelation. For in the very act of
+          resistance, he unearthed the paradox that had eluded him—a revelation
+          that the most profound beauty often resides in the surrender to life's{" "}
+          <WordModal boundingRef={demoRef} word="ineffable">
+            <Dictionary
+              word="ineffable"
+              pronunciation="/ɪnˈɛfəbəl/"
+              type="adjective"
+              meaning="too great or extreme to be expressed or described in words"
+              usage="The beauty of the sunset was ineffable, leaving us speechless."
+            />
+          </WordModal>{" "}
           grace.
         </div>
       </div>
@@ -98,21 +160,20 @@ const WordModalPage = () => {
   );
 };
 
-const Dictionary = ({
-  word = "Harsh",
-  pronounciation = "s",
-  type = "s",
-  usage = "ssdfaksgfas kldjhfljashdflhasdlfhslkjdhfkj uhs ldiufhlHsild ushd lfuihSD LFHLsh  SHILD",
-}) => {
+const Dictionary = ({ word, pronunciation, type, usage, meaning }) => {
   return (
-    <span className="dictionary-wrap">
-      <small className="dictionary-word">{word}</small>
+    <div className="dictionary-wrap">
+      <p className="dictionary-word">{word}</p>
 
-      <small className="dictionary-pronounciation">{pronounciation}</small>
+      <div className="dictionary-details-bar">
+        <Speech size={18} strokeWidth={1.8} />
+        <p className="dictionary-pronounciation">{pronunciation}</p>•
+        <p className="dictionary-type">{type}</p>
+      </div>
 
-      <small className="dictionary-type">{type}</small>
-      <small className="dictionary-usage">{usage}</small>
-    </span>
+      <p className="dictionary-meaning">{meaning}</p>
+      {usage && <p className="dictionary-usage">"{usage}"</p>}
+    </div>
   );
 };
 
